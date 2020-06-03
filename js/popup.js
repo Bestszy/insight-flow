8145ea29ff89b8541e95e1f1660086a6e5ab609b
Bart Koćmierowski <bartoszkocmierowski@gmail.com>
2020-06-03 22:04:32 +0200

    add

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,11 @@
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?id=3088825&appid=4e9669d0699143b043c5249aefb30374", 
 function(data){
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    console.log(icon)
+    $('.icon').attr('src', icon)
+    $('.description').append(data.weather[0].description)
+    $('.temp').append(data.main.temp)
+    $('.humidity').append(data.main.humidity)
+    $('num').append(data.weather[0].main)
+    console.log(data)
 
 })
